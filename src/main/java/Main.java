b6d307935ea50c6842fa44b05c32f4c8b684bd73
piotriwaniuk <piekoszek@gmail.com>
2021-04-24 19:29:34 +0200

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public int metoda() {
-//        if( 1 == 1)
-//        throw new RuntimeException();
+        if( 1 == 1)
+        throw new RuntimeException();
         return 10;
     }
 
