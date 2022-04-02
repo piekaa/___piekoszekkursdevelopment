e1d3ef6e68926a2ac1379182405c3bc9b17282f4
piotriwaniuk <piekoszek@gmail.com>
2022-04-02 16:36:41 +0200

    Quizz Template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,6 @@
 public class Main {
 
-    public int metoda() {
-//        if( 1 == 1)
-//        throw new RuntimeException();
-        return 10;
-    }
-
     public static void main(String[] args) {
-        System.out.println("dupa");
-    }
 
+    }
 }
