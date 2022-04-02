44b9f2e23c61ab4f7b67c4a7ee8afdff5a6e1fbd
piotriwaniuk <piekoszek@gmail.com>
2022-04-02 16:48:05 +0200

    quiz 0

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a - b;
+    }
+
+    public int subtract(int a, int b) {
+        return 0;
+    }
+
+    public int square(int n) {
+        return n * 2;
+    }
+
+    public float multiply(float a, float b) {
+        return 0;
+    }
+
+    public float divide(int a, int b) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/PiekoszekCalculatorTest.java
@@ -0,0 +1,15 @@
+import extension.PiekoszekExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(PiekoszekExtension.class)
+public class PiekoszekCalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    void testAdd() {
+
+
+
+    }
+
+}
