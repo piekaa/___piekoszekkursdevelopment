fe45b046090062a4c0649a6a60ce18c6a3fd2852
piotriwaniuk <piekoszek@gmail.com>
2022-06-03 16:16:43 +0200

    quiz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
+
+        System.out.println("Powinno być");
+        System.out.println("Good morning, Janusz");
+        System.out.println("Jest");
         System.out.println(greeter.greet("Janusz"));
     }
 }
