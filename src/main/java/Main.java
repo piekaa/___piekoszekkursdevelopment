ccf40e26a5af60e44ae1c4bce9b6a73bbb714e51
piotriwaniuk <piekoszek@gmail.com>
2022-07-02 10:17:45 +0200

    quiz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Counter counter = new Counter();
+
         System.out.print("Powinno być: ");
-        System.out.println();
+        System.out.println("1 2 3 4 5");
 
         System.out.print("       Jest: ");
-        System.out.println();
+        System.out.println(counter.count(5));
     }
 }
