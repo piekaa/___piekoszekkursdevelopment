67385d70cb160c0468e801c7443d2a35e56d39ad
piotriwaniuk <piekoszek@gmail.com>
2022-07-02 15:06:05 +0200

    ?

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Zaimplementuj metodę `check` w klasie `AdultChecker`
 tak, aby w zależności od parametru `age` zwracała
 `"adult"` lub `"not adult"`. Zakładamy, że dorosły,
 to ten, co ma 18 lat.
+
+#### Przykłady
+`check(5)` -> `"not adult"`
+
+`check(60)` -> `"adult"`
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
         var adultChecker = new AdultChecker();
 
-        System.out.print("Powinno być: Adult");
-        System.out.println();
+        System.out.print("Powinno być: ");
+        System.out.println("Adult");
 
         System.out.print("       Jest: ");
         System.out.println(adultChecker.check(18));
