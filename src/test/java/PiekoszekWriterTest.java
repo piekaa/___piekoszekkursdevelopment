1882ff98ed1c80d6455108a2c0ba40b62496b5ff
piotriwaniuk <piekoszek@gmail.com>
2022-07-04 18:37:30 +0200

    ?

--- src/test/java/PiekoszekWriterTest.java
+++ src/test/java/PiekoszekWriterTest.java
@@ -4,7 +4,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
